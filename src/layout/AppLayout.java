7891783fdc787e84b7f9be3d4d833774ac8678ea
Alcir Daluz <alcirdaluz@protonmail.com>
2023-12-20 16:53:42 -0300

    Indentação do arquivo de exercício AppLayout

--- src/layout/AppLayout.java
+++ src/layout/AppLayout.java
@@ -7,25 +7,25 @@ import javafx.stage.Stage;
 
 public class AppLayout extends Application {
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-		Parent raiz = null;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent raiz = null;
 //		raiz = new TesteAnchorPane();
 //		raiz = new TesteBorderPane();
 //		raiz = new TesteFlowPane();
 //		raiz = new TesteGridPane();
 //		raiz = new TesteStackPane();
-		raiz = new TesteTilePane();
+        raiz = new TesteTilePane();
 
-		Scene principal = new Scene(raiz, 800, 600);
+        Scene principal = new Scene(raiz, 800, 600);
 
-		primaryStage.setScene(principal);
-		primaryStage.setTitle("Gerenciadores de Layout");
-		primaryStage.show();
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
+        primaryStage.setScene(principal);
+        primaryStage.setTitle("Gerenciadores de Layout");
+        primaryStage.show();
+    }
 }
