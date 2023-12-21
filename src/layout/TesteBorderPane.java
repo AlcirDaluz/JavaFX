85ae1057a7110024dc1b8785fda5b9797f2885dd
Alcir Daluz <alcirdaluz@protonmail.com>
2023-12-21 17:10:59 -0300

    Indentação do arquivo de exercício TesteBorderPane

--- src/layout/TesteBorderPane.java
+++ src/layout/TesteBorderPane.java
@@ -4,21 +4,21 @@ import javafx.scene.layout.BorderPane;
 
 public class TesteBorderPane extends BorderPane {
 
-	public TesteBorderPane() {
+    public TesteBorderPane() {
 
-		Caixa c1 = new Caixa().comTexto("Topo");
-		setTop(c1);
+        Caixa c1 = new Caixa().comTexto("Topo");
+        setTop(c1);
 
-		Caixa c2 = new Caixa().comTexto("Esquerda");
-		setLeft(c2);
+        Caixa c2 = new Caixa().comTexto("Esquerda");
+        setLeft(c2);
 
-		Caixa c3 = new Caixa().comTexto("Direita");
-		setRight(c3);
+        Caixa c3 = new Caixa().comTexto("Direita");
+        setRight(c3);
 
-		Caixa c4 = new Caixa().comTexto("Fundo");
-		setBottom(c4);
+        Caixa c4 = new Caixa().comTexto("Fundo");
+        setBottom(c4);
 
-		Caixa c5 = new Caixa().comTexto("Área Central");
-		setCenter(c5);
-	}
+        Caixa c5 = new Caixa().comTexto("Área Central");
+        setCenter(c5);
+    }
 }
