fdad103d5ff039f080ff6c912e59641858779cc0
Alcir Daluz <alcirdaluz@protonmail.com>
2023-12-21 21:27:10 -0300

    Indentação do arquivo de exercício TesteFlowPane #02

--- src/layout/AppLayout.java
+++ src/layout/AppLayout.java
@@ -14,12 +14,12 @@ public class AppLayout extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent raiz = null;
-//		raiz = new TesteAnchorPane();
-//		raiz = new TesteBorderPane();
-//		raiz = new TesteFlowPane();
-//		raiz = new TesteGridPane();
-//		raiz = new TesteStackPane();
+        Parent raiz;
+        raiz = new TesteAnchorPane();
+        raiz = new TesteBorderPane();
+        raiz = new TesteFlowPane();
+        raiz = new TesteGridPane();
+        raiz = new TesteStackPane();
         raiz = new TesteTilePane();
 
         Scene principal = new Scene(raiz, 800, 600);
