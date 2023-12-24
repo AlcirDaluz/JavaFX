109f6eaa3e717bb2f7f1f2e43f8ec01eeba0944d
Alcir Daluz <alcirdaluz@protonmail.com>
2023-12-24 16:13:56 -0300

    Indentação do arquivo de exercício TesteTilePane

--- src/layout/TesteTilePane.java
+++ src/layout/TesteTilePane.java
@@ -1,29 +1,29 @@
 package layout;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.layout.TilePane;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TesteTilePane extends TilePane {
 
-	public TesteTilePane() {
+    public TesteTilePane() {
 
-		List<Quadrado> quadrados = new ArrayList<>();
+        List<Quadrado> quadrados = new ArrayList<>();
 
-		for (int i = 1; i < 10; i++) {
-			quadrados.add(new Quadrado(i * 10));
-		}
+        for (int i = 1; i < 10; i++) {
+            quadrados.add(new Quadrado(i * 10));
+        }
 
-		setPadding(new Insets(20, 10, 20, 10));
-		setVgap(10);
-		setHgap(10);
-		setOrientation(Orientation.VERTICAL);
-		setTileAlignment(Pos.BOTTOM_RIGHT);
+        setPadding(new Insets(20, 10, 20, 10));
+        setVgap(10);
+        setHgap(10);
+        setOrientation(Orientation.VERTICAL);
+        setTileAlignment(Pos.BOTTOM_RIGHT);
 
-		getChildren().addAll(quadrados);
-	}
+        getChildren().addAll(quadrados);
+    }
 }
