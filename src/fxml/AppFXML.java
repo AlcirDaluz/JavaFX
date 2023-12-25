ca44e882d8e55acb2851388deb7e245b58185fb1
Alcir Daluz <alcirdaluz@protonmail.com>
2023-12-25 00:02:23 -0300

    Aula 390: FXML Config

---
+++ src/fxml/AppFXML.java
@@ -0,0 +1,17 @@
+package fxml;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppFXML extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.show();
+    }
+}
