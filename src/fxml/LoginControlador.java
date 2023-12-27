178142491ef04b5aa4f3935856ada5682e6c8252
Alcir Daluz <alcirdaluz@protonmail.com>
2023-12-27 16:06:00 -0300

    Aula 396: Usando ControlsFX

--- src/fxml/LoginControlador.java
+++ src/fxml/LoginControlador.java
@@ -1,8 +1,10 @@
 package fxml;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.controlsfx.control.Notifications;
 
 public class LoginControlador {
 
@@ -12,7 +14,14 @@ public class LoginControlador {
     private PasswordField campoSenha;
 
     public void entrar() {
-        System.out.println(campoEmail.getText());
-        System.out.println(campoSenha.getText());
+
+        boolean emailValido = campoEmail.getText().equals("aluno@cod3r.com.br");
+        boolean senhaValida = campoSenha.getText().equals("12345678");
+
+        if (emailValido && senhaValida) {
+            Notifications.create().position(Pos.TOP_RIGHT).title("Login FXML").text("Login efetuado com sucesso!").showInformation();
+        } else {
+            Notifications.create().position(Pos.TOP_RIGHT).title("Login FXML").text("Usuário e Senha inválidos!").showError();
+        }
     }
 }
